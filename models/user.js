b9decd621d39b1b211f9d9f9dc7bb3b60b10e707
GR8 <eminhwan12@gmail.com>
2021-03-09 16:32:50 +0900

    test Using jest coverage

--- models/user.js
+++ models/user.js
@@ -37,7 +37,7 @@ module.exports = class User extends Sequelize.Model{
         });
     }
 
-    static assciate(db){
+    static associate(db){
         db.User.hasMany(db.Post);
         db.User.belongsToMany(db.User, {
             foreignKey: 'followingId',
